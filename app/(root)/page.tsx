c42eb1374b581ecdecdf1ec7621c3ab26df45b4c
ghost-love-you <1756613825@qq.com>
2024-09-09 16:45:28 +0800

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Home;
