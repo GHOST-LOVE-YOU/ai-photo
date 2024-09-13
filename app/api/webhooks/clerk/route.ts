d35c9ce3917a25c770cb6c1c48f4bb279f4c238f
ghost-love-you <1756613825@qq.com>
2024-09-13 21:19:53 +0800

    add clerk webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,58 +57,58 @@ export async function POST(req: Request) {
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
 
-  // CREATE
-  if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } =
-      evt.data;
+  // // CREATE
+  // if (eventType === "user.created") {
+  //   const { id, email_addresses, image_url, first_name, last_name, username } =
+  //     evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name ?? "",
-      lastName: last_name ?? "",
-      photo: image_url,
-    };
+  //   const user = {
+  //     clerkId: id,
+  //     email: email_addresses[0].email_address,
+  //     username: username!,
+  //     firstName: first_name ?? "",
+  //     lastName: last_name ?? "",
+  //     photo: image_url,
+  //   };
 
-    const newUser = await createUser(user);
+  //   const newUser = await createUser(user);
 
-    // Set public metadata
-    if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id,
-        },
-      });
-    }
+  //   // Set public metadata
+  //   if (newUser) {
+  //     await clerkClient.users.updateUserMetadata(id, {
+  //       publicMetadata: {
+  //         userId: newUser._id,
+  //       },
+  //     });
+  //   }
 
-    return NextResponse.json({ message: "OK", user: newUser });
-  }
+  //   return NextResponse.json({ message: "OK", user: newUser });
+  // }
 
-  // UPDATE
-  if (eventType === "user.updated") {
-    const { id, image_url, first_name, last_name, username } = evt.data;
+  // // UPDATE
+  // if (eventType === "user.updated") {
+  //   const { id, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      firstName: first_name ?? "",
-      lastName: last_name ?? "",
-      username: username!,
-      photo: image_url,
-    };
+  //   const user = {
+  //     firstName: first_name ?? "",
+  //     lastName: last_name ?? "",
+  //     username: username!,
+  //     photo: image_url,
+  //   };
 
-    const updatedUser = await updateUser(id, user);
+  //   const updatedUser = await updateUser(id, user);
 
-    return NextResponse.json({ message: "OK", user: updatedUser });
-  }
+  //   return Response.json({ message: "OK", user: updatedUser });
+  // }
 
-  // DELETE
-  if (eventType === "user.deleted") {
-    const { id } = evt.data;
+  // // DELETE
+  // if (eventType === "user.deleted") {
+  //   const { id } = evt.data;
 
-    const deletedUser = await deleteUser(id!);
+  //   const deletedUser = await deleteUser(id!);
 
-    return NextResponse.json({ message: "OK", user: deletedUser });
-  }
+  //   return Response.json({ message: "OK", user: deletedUser });
+  // }
 
   return new Response("", { status: 200 });
 }
